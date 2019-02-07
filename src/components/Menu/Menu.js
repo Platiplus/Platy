3ef764d98e93a63252c419b065eaf2908b10743e
Victor Rosa <victor@m2mtelemetria.com.br>
2019-02-07 17:52:17 -0200

    Menu toggle behaviour on click implemented as well as style changes

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,27 +1,37 @@
 import React, {Component} from 'react';
-import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+import SideNav, { NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Dashboard from '../../pages/Dashboard/Dashboard';
 import Transactions from '../../pages/Transactions/Transactions';
 import './Menu.scss';
 
 export default class Menu extends Component{
+    state = {
+        expanded: false
+    };
+
   render(){
     return(
       <Router>
       <Route render={({ location, history }) => (
           <React.Fragment>
-              <SideNav onSelect={(selected) => {
-                  const to = '/' + selected;
-                  if (location.pathname !== to) {
-                      history.push(to);
+              <SideNav 
+                expanded={this.state.expanded}
+                onToggle={() => {
+                    this.setState({ expanded : !this.state.expanded});
+                }}
+                onSelect={(selected) => {
+                    this.setState({expanded : false});
+                    const to = '/' + selected;
+                    if (location.pathname !== to) {
+                        history.push(to);
                     }
                 }}>
                   <SideNav.Toggle />
                   <SideNav.Nav defaultSelected="dashboard">
                       <NavItem eventKey="dashboard">
                           <NavIcon>
-                              <i className="fa fa-fw fa-line-chart" style={{ fontSize: '1.75em' }} />
+                              <i className="fa fa-line-chart" style={{ fontSize: '1.75em', paddingTop: '15px' }} />
                           </NavIcon>
                           <NavText>
                               Dashboard
@@ -29,7 +39,7 @@ export default class Menu extends Component{
                       </NavItem>
                       <NavItem eventKey="transactions">
                           <NavIcon>
-                              <i className="fa fa-fw fa-money" style={{ fontSize: '1.75em' }} />
+                              <i className="fa fa-money" style={{ fontSize: '1.75em', paddingTop: '15px' }} />
                           </NavIcon>
                           <NavText>
                               Transactions

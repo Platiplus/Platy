768fbd01d37637b7a8217ea8281f9c26bd2a9480
Platiplus <quazarweb@gmail.com>
2019-02-27 17:25:39 -0300

    Improved state changes by not manipulating state directly

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -61,37 +61,38 @@ export default class Dashboard extends Component{
       </div>);
   };
 
-  loadStatsCards(context){
+  loadStatsCards(){
     let card_calls = [];
     
-    context.cards_urls.map((url) => {
-      card_calls.push(axios.post(process.env.REACT_APP_API + url, {owner: context.owner}));  
+    this.cards_urls.map((url) => {
+      card_calls.push(axios.post(process.env.REACT_APP_API + url, {owner: this.owner}));  
     });
 
     Promise.all(card_calls).then((cards) => {
+      let new_cards = [];
       cards.forEach((card) => {
-        context.state.cards.push(card['data']['data']);
+        new_cards.push(card['data']['data']);
       })
-      context.setState(context.state.cards);
+      this.setState({cards: new_cards});
     });
   };
   
-  loadChartCards(context){
-    axios.post(process.env.REACT_APP_API + context.flux_chart_url, {owner: context.owner}).then((chart_data) => {
-      context.state.flux = <LineChart info={{header: 'Fluxo de Caixa', description: 'Últimos 6 meses', chart: chart_data['data']['data']}}/>
-      context.setState(this.state.flux);
+  loadChartCards(){
+    axios.post(process.env.REACT_APP_API + this.flux_chart_url, {owner: this.owner}).then((chart_data) => {
+      let flux = <LineChart info={{header: 'Fluxo de Caixa', description: 'Últimos 6 meses', chart: chart_data['data']['data']}}/>
+      this.setState({flux});
     });
 
-    axios.post(process.env.REACT_APP_API + context.compare_chart_url, {owner: context.owner}).then((chart_data) => {
-      context.state.compare = <BarChart info={{header: 'Comparativo mês a mês', description: 'Mês passado e atual', chart: chart_data['data']['data']}}/>
-      context.setState(this.state.compare);
+    axios.post(process.env.REACT_APP_API + this.compare_chart_url, {owner: this.owner}).then((chart_data) => {
+      let compare = <BarChart info={{header: 'Comparativo mês a mês', description: 'Mês passado e atual', chart: chart_data['data']['data']}}/>
+      this.setState({compare});
     });
   };
 
-  loadTransactionCard(context){
-    axios.post(process.env.REACT_APP_API + context.last_transactions_url, {owner: context.owner}).then((last_transactions) => {
-      context.state.last_transactions = <DashboardTable data={last_transactions['data']['data']}/>
-      context.setState(this.state.last_transactions);
+  loadTransactionCard(){
+    axios.post(process.env.REACT_APP_API + this.last_transactions_url, {owner: this.owner}).then((last__made_transactions) => {
+      let last_transactions = <DashboardTable data={last__made_transactions['data']['data']}/>
+      this.setState({last_transactions});
     });
   };
 }
